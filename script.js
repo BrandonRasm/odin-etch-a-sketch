c845b2a1343a4c840b51bf2cc0517b896fe99eb2
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-12 21:29:22 -0800

    moved all the grid creation code to a function to make it cleaner

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const container = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    const temp = document.createElement("div");
-    temp.classList.add("grid");
+CreateNewGrid(container);
 
-    temp.addEventListener("mouseover", function hover() {
-        temp.classList.add("hovered");
-    });
 
-    // temp.addEventListener("mouseout", function unhover() {
-    //     temp.classList.remove("hovered");
-    // }); 
 
-    container.appendChild(temp);
+
+function CreateNewGrid(container) {
+    for (let i = 0; i < 16; i++) {
+        const temp = document.createElement("div");
+        temp.classList.add("grid");
+
+        temp.addEventListener("mouseover", function hover() {
+            temp.classList.add("hovered");
+        });
+
+        // temp.addEventListener("mouseout", function unhover() {
+        //     temp.classList.remove("hovered");
+        // }); 
+
+        container.appendChild(temp);
+    }
 }
-
