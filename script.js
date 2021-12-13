f7f0ce5d91b3fa6b362f51f109fc3066c25aca5c
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-12 23:03:25 -0800

    updated the createGrid function to dynamically size them based on how many

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const container = document.querySelector(".container");
 const clearButton = document.querySelector("button");
-CreateNewGrid(container);
+CreateNewGrid(container, 16);
 
 clearButton.addEventListener("click", () => {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
-    prompt("TEST");
+    let gridWidth = 0;
+    while (gridWidth == 0) {
+        const tempWidth = prompt("Select squares per side", "Max 100");
+        if (tempWidth < 101 && tempWidth > 0 && !isNaN(tempWidth)) {
+            gridWidth = tempWidth;
+        }
+
+    }
+    CreateNewGrid(container, gridWidth)
 });
 
 
-function CreateNewGrid(container) {
-    for (let i = 0; i < 256; i++) {
+function CreateNewGrid(container, gridWidth) {
+    for (let i = 0; i < gridWidth * gridWidth; i++) {
         const temp = document.createElement("div");
-        temp.classList.add("grid");
+        const boxDim = 512 / gridWidth;
+        temp.style.height  = temp.style.width = (boxDim + "px");
+        
 
+
+        temp.classList.add("grid");
         temp.addEventListener("mouseover", function hover() {
             temp.classList.add("hovered");
         });
