f2cabdc61c74c32d72db6291c9818453ac0af7c0
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-12 20:59:52 -0800

    CSS: added style sheet added styles to each class
    JAVASCRIPT:
    added a .grid class to each div in the for loop

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
     const temp = document.createElement("div");
+    temp.classList.add("grid") ;
     container.appendChild(temp);
 }
