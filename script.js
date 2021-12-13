a357577580339777c3a681084dff98a242fce751
Brandon Rasmussen <BrandonRasm0412@gmail.com>
2021-12-12 21:13:34 -0800

    CSS added hovered style to highlight the grid black
    JAVASCRIPT
    added event listeners to grid for mouseover and mouse out to create
    hover effect

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector(".container");
 for (let i = 0; i < 16; i++) {
     const temp = document.createElement("div");
-    temp.classList.add("grid") ;
+    temp.classList.add("grid");
+
+    temp.addEventListener("mouseover", function hover() {
+        temp.classList.add("hovered");
+    });
+
+    temp.addEventListener("mouseout", function unhover() {
+        temp.classList.remove("hovered");
+    }); 
+
     container.appendChild(temp);
 }
+
